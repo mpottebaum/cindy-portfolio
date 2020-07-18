3c46b2fae2a378215686cbd6f4416ac96c771724
mpottebaum <mpottebaum@gmail.com>
2020-07-18 15:42:32 -0500

    built menu click listener

--- app/javascript/index.js
+++ app/javascript/index.js
@@ -1,3 +1,26 @@
 document.addEventListener('DOMContentLoaded', () => {
-    console.log('loaded')
+    menuListener()
 })
+
+const menuListener = () => {
+    const menuButton = document.getElementById('menu-btn')
+    menuButton.addEventListener('click', () => {
+        const menu = document.getElementById('menu')
+        if(menuButton.dataset.status === 'closed') {
+            const newMenuItems = buildMenuItems()
+            menu.appendChild(newMenuItems)
+            menuButton.dataset.status = 'open'
+        } else {
+            const oldMenuItems = document.getElementById('menu-items')
+            menu.removeChild(oldMenuItems)
+            menuButton.dataset.status = 'closed'
+        }
+    })
+}
+
+const buildMenuItems = () => {
+    const items = document.createElement('div')
+    items.id = 'menu-items'
+    items.innerHTML = '<p>hey</p>'
+    return items
+}
